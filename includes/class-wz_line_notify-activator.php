97dee48ec3438b0872fccf16dccd3b6a708bd847
Nuttaruj <nuttaruj@gmail.com>
2018-07-19 16:48:38 +0700

    Initial commit

---
+++ includes/class-wz_line_notify-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://walnutztudio.com
+ * @since      1.0.0
+ *
+ * @package    Wz_line_notify
+ * @subpackage Wz_line_notify/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Wz_line_notify
+ * @subpackage Wz_line_notify/includes
+ * @author     WalnutZtudio <walnutztudio@gmail.com>
+ */
+class Wz_line_notify_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ public/partials/wz_line_notify-public-display.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Provide a public-facing view for the plugin
+ *
+ * This file is used to markup the public-facing aspects of the plugin.
+ *
+ * @link       https://walnutztudio.com
+ * @since      1.0.0
+ *
+ * @package    Wz_line_notify
+ * @subpackage Wz_line_notify/public/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
